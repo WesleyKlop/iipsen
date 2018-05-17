186d053cc321efdce53d7e74752c23d58308a081
Ewoutmullink <39297966+Ewoutmullink@users.noreply.github.com>
2018-05-17 11:28:41 +0200

    Delete spelerLijst.java

--- src/game/player/spelerLijst.java
+++
@@ -1,9 +0,0 @@
-package game.player;
-
-public class spelerLijst{
-	static Speler[] spelerLijst = new Speler[3];
-    
-	public static void voegSpelertoe(String naam, String kleur) {
-		spelerLijst[0] = new Speler(naam,kleur);
-	}
-}
