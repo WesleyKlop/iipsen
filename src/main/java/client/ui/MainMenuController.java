190501c834278423c13f8286ae287a12480639c4
Thom Leenman <thomleenman@gmail.com>
2018-05-29 11:24:04 +0200

    Better theme in the main menu. Trains change colour everytime they pass by. Imported custom font. Mute button in the main menu. Exit button in the main menu.

--- src/main/java/client/ui/MainMenuController.java
+++ src/main/java/client/ui/MainMenuController.java
@@ -1,7 +1,6 @@
 package client.ui;
 
 
-import javafx.animation.Animation;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -9,6 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -18,14 +18,12 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -69,31 +67,39 @@ public class MainMenuController implements Initializable {
     public ImageView train2;
     public ImageView train3;
 
+    public MediaPlayer player;
+
     @Override
     public void initialize(URL url, ResourceBundle bundle) {
-        playMusic("/sound/background.mp3");
+        playMusic();
         style(VBoxMain);
         style(VBoxPlay);
         style(VBoxLoad);
         style(HBoxRule1);
         style(HBoxRule2);
         style(HBoxRule3);
-        ruleRules.setFont(Font.font("Californian FB", 20));
         ImageView[] trains = {train1, train2, train3};
         trainAnimation(trains);
-
+        ruleRules.setFont(Font.loadFont(getClass().getResourceAsStream("/fonts/MavenPro-Regular.ttf"), 25));
         rootPane.setStyle("-fx-background-color: linear-gradient(to bottom, #bfe8f9 0%,#0082ED 70%);");
     }
 
+
+    private void trainAnimation(ImageView image) {
+        image.setImage(new Image(getClass().getResource(generateTrainColorImage()).toString()));
+        TranslateTransition trainAni = new TranslateTransition(Duration.seconds(20), image);
+        trainAni.setByX(5000);
+        trainAni.play();
+        trainAni.setOnFinished(e -> {
+            image.setLayoutX(image.getLayoutX() - 5000);
+            trainAnimation(image);
+        });
+    }
     private void trainAnimation(ImageView[] image) {
-        for (int i = 0; i < image.length; i++) {
-            TranslateTransition trainAnimation = new TranslateTransition(Duration.seconds(30), image[i]);
-            trainAnimation.setToX(3100);
-            trainAnimation.setCycleCount(Animation.INDEFINITE);
-            trainAnimation.play();
+        for (ImageView train : image) {
+            trainAnimation(train);
         }
     }
-
     private String generateTrainColorImage() {
         int imageNumber = (int) (Math.random() * 5);
         String path = "/images/train_";
@@ -120,50 +126,26 @@ public class MainMenuController implements Initializable {
         return path;
     }
 
-    public void manageRules(MouseEvent mouseEvent) {
-        Label label = (Label) mouseEvent.getSource();
-        String rules = getRule(label);
-        displayRules(rules);
-    }
-
-    private String getRule(Label label) {
-        // Gets label from mouseEvent
-        String labelId = label.getId();
-        String rules = "";
-        try {
-            File ruleFile = new File("/string/rules.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document rulesDoc = dBuilder.parse(ruleFile);
-            rulesDoc.getDocumentElement().normalize();
-
-            rules = rulesDoc.getElementsByTagName(labelId).item(0).getTextContent();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return rules;
-    }
-
-    private void displayRules(String rules) {
-        // Sets rules scrollpane text to given text
-        ruleRules.setText(rules);
-    }
-
-    private void playMusic(String name) {
-        Media backgroundMusic = new Media(getClass().getResource(name).toString());
-        MediaPlayer player = new MediaPlayer(backgroundMusic);
+    private void playMusic() {
+        Media backgroundMusic = new Media(getClass().getResource("/sound/background.mp3").toString());
+        player = new MediaPlayer(backgroundMusic);
         player.volumeProperty().bind(optionVolumeSlider.valueProperty());
         player.setCycleCount(MediaPlayer.INDEFINITE);
         player.play();
     }
 
+    public void mute() {
+        player.setMute(!player.isMute());
+        optionVolumeSlider.setDisable(player.isMute());
+    }
+
     private void style(Label label) {
-        label.setFont(Font.font("Californian FB", FontWeight.BOLD, 25));
-        label.setStyle("-fx-background-color: white;" + "-fx-border-color: black;");
+        Font font = Font.loadFont(getClass().getResourceAsStream("/fonts/MavenPro-Medium.ttf"), 25);
+        label.setFont(font);
+        label.setStyle("-fx-background-color: white;" + "-fx-background-radius: 10;");
         label.setAlignment(Pos.CENTER);
         label.setTextFill(Color.BLACK);
-        label.setPadding(new Insets(12, 12, 12, 12));
+        label.setPadding(new Insets(10));
         label.setPrefHeight(50);
         label.setPrefWidth(250);
     }
@@ -188,6 +170,35 @@ public class MainMenuController implements Initializable {
     public void hoverExit(MouseEvent mouseEvent) {
         Label label = (Label) mouseEvent.getSource();
         label.setTextFill(Color.BLACK);
+    }
+
+    public void manageRules(MouseEvent mouseEvent) {
+        Label label = (Label) mouseEvent.getSource();
+        String rules = getRule(label);
+        ruleRules.setText(rules);
+    }
+
+    private String getRule(Label label) {
+        String labelId = label.getId();
+        String rules = "";
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document rulesDoc = builder.parse(getClass().getResourceAsStream("/string/rules.xml"));
+            rulesDoc.getDocumentElement().normalize();
+
+            String[] arr = rulesDoc.getElementsByTagName(labelId).item(0).getTextContent().split("\n");
+            StringBuilder buffer = new StringBuilder();
+            for (String line : arr) {
+                buffer.append(line.trim());
+                buffer.append("\n");
+            }
+            rules = buffer.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rules;
     }
 
     public void openMenuSequence(MouseEvent mouseEvent) {
@@ -274,4 +285,17 @@ public class MainMenuController implements Initializable {
         System.exit(0);
     }
 
+    public void widthUpImageView(MouseEvent mouseEvent) {
+        ImageView source = (ImageView) mouseEvent.getSource();
+        source.setFitWidth(source.getFitWidth() + 14);
+        source.setLayoutX(source.getLayoutX() - 7);
+        source.setLayoutY(source.getLayoutY() - 7);
+    }
+
+    public void widthDownImageView(MouseEvent mouseEvent) {
+        ImageView source = (ImageView) mouseEvent.getSource();
+        source.setFitWidth(source.getFitWidth() - 14);
+        source.setLayoutX(source.getLayoutX() + 7);
+        source.setLayoutY(source.getLayoutY() + 7);
+    }
 }
