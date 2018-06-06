cd866f7a44f5eb98bda72189e6fd7d0702447bad
ewout <ewout-willem@hotmail.com>
2018-06-06 14:31:56 +0200

    Update screen

--- src/main/java/client/ui/PlayscreenController.java
+++ src/main/java/client/ui/PlayscreenController.java
@@ -42,18 +42,17 @@ public class PlayscreenController implements Initializable {
 
     public void orangeroadEntered(MouseEvent event) {
         title1.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
-        title2.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
-        title3.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
-        title4.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
+        title1.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
+        title1.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
+        title1.setStyle("-fx-background-color: orange; -fx-border-color:white; -fx-border-width: 1; -fx-border-style: solid;");
     }
 
     public void orangeroadExited(MouseEvent event) {
-        title1.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
         title2.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
-        title3.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
-        title4.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
+        title2.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
+        title2.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
+        title2.setStyle("-fx-background-color: orange; -fx-border-color:black; -fx-border-width: 1; -fx-border-style: solid;");
     }
-
 
     public void initialize(URL url, ResourceBundle rb) {
         bPane.setBackground(new Background(myBI));
