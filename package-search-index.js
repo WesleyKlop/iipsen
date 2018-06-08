0417103a27b8c467add75d52baafd1bb522937de
Wesley Klop <wesley19097@gmail.com>
2018-06-08 14:24:39 +0200

    Update javadoc

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"client"},{"l":"client.ui"},{"l":"client.ui.components"},{"l":"game"},{"l":"game.actions"},{"l":"game.cards"},{"l":"game.player"},{"l":"game.routecards"},{"l":"server"},{"l":"util"}]
+packageSearchIndex = [{"l":"client"},{"l":"client.ui"},{"l":"client.ui.components"},{"l":"client.ui.factories"},{"l":"game"},{"l":"game.actions"},{"l":"game.cards"},{"l":"game.player"},{"l":"game.routecards"},{"l":"server"},{"l":"util"}]
