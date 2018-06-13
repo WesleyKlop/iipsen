db892aad4d73d35c322c37a46defe78f07936743
Mark Bueno <markbuenoguzman@hotmail.com>
2018-06-13 16:20:07 +0200

    playerbox game gemaakt

--- src/main/java/client/ui/controllers/LayoutCardController.java
+++ src/main/java/client/ui/controllers/LayoutCardController.java
@@ -1,17 +1,13 @@
 package client.ui.controllers;
 
 
-import game.GameStoreProvider;
-import game.actions.RandomCardAction;
 import game.player.Player;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 
 import java.net.URL;
-import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
 
@@ -48,13 +44,5 @@ public class LayoutCardController implements Initializable {
         white.setVisible(false);
         yellow.setVisible(false);
         locomotive.setVisible(false);
-        Player player = new Player("jan", Color.RED);
-        var action = new RandomCardAction(player);
-        try {
-            GameStoreProvider.sendAction(action);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        System.out.println(player.getCardStack());
     }
 }
---
+++ src/main/java/client/ui/views/LayoutGamePlayerbox.java
@@ -0,0 +1,20 @@
+package client.ui.views;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LayoutGamePlayerbox implements Initializable {
+    @FXML
+    VBox vbox;
+    @FXML
+    Label playerNameLabel;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
