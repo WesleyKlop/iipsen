c7e94c19142ebd7457a3672ce69acb5e3fec5d91
ewout <ewout-willem@hotmail.com>
2018-06-13 12:35:20 +0200

    update

--- src/main/java/client/ui/controllers/LayoutCardController.java
+++ src/main/java/client/ui/controllers/LayoutCardController.java
@@ -1,16 +1,22 @@
 package client.ui.controllers;
 
 
+import game.GameStoreProvider;
+import game.actions.RandomCardAction;
+import game.player.Player;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
 
 public class LayoutCardController implements Initializable {
+    Player player;
     @FXML
     HBox HBox;
     @FXML
@@ -42,7 +48,13 @@ public class LayoutCardController implements Initializable {
         white.setVisible(false);
         yellow.setVisible(false);
         locomotive.setVisible(false);
+        Player player = new Player("jan", Color.RED);
+        var action = new RandomCardAction(player);
+        try {
+            GameStoreProvider.sendAction(action);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        System.out.println(player.getCardStack());
     }
-
-
 }
