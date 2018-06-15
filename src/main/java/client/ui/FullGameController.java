40ed42f87f4f1a30c9835d8373ac681a9704a8ec
Mark Bueno <markbuenoguzman@hotmail.com>
2018-06-15 11:43:13 +0200

    PlayerBox gekoppeld aan kleur en naam

--- src/main/java/client/ui/FullGameController.java
+++ src/main/java/client/ui/FullGameController.java
@@ -1,15 +1,25 @@
 package client.ui;
 
+import client.ui.views.LayoutGamePlayerbox;
+import game.GameStore;
+import game.GameStoreProvider;
+import game.player.Player;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
+import java.util.List;
+
 public class FullGameController {
 
+    GameStore gameStore;
     @FXML
     private Pane routesMap, rootPane;
+    @FXML
+    private LayoutGamePlayerbox playerBoxController;
+    private List<Player> players;
 
     public void initialize() {
-
+        players = GameStoreProvider.getStore().getPlayers();
+        playerBoxController.setPlayers(players);
     }
-
 }
--- src/main/java/client/ui/views/LayoutGamePlayerbox.java
+++ src/main/java/client/ui/views/LayoutGamePlayerbox.java
@@ -1,20 +1,53 @@
 package client.ui.views;
 
+import game.player.Player;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class LayoutGamePlayerbox implements Initializable {
+
+    private static final Logger Log = LogManager.getLogger(LayoutGamePlayerbox.class);
     @FXML
     VBox vbox;
     @FXML
-    Label playerNameLabel;
+    VBox background1;
+    @FXML
+    VBox background2;
+    @FXML
+    VBox background3;
+    @FXML
+    Label playerNameLabel1;
+    @FXML
+    Label playerNameLabel2;
+    @FXML
+    Label playerNameLabel3;
+
+    Label[] labels;
+    VBox[] backgrounds;
+
 
     public void initialize(URL location, ResourceBundle resources) {
+        labels = new Label[]{playerNameLabel1, playerNameLabel2, playerNameLabel3};
+        backgrounds = new VBox[]{background1, background2, background3};
+    }
 
+
+    public void setPlayers(List<Player> players) {
+        for (int i = 0; i < players.size(); i++) {
+            labels[i].setText(players.get(i).getPlayerName());
+            backgrounds[i].setBackground(new Background(new BackgroundFill(players.get(i).getColorAsColor(), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
     }
 }
