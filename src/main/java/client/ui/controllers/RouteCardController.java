11f2af420dead63bff920fdeb43c68f140582ab8
ewout <ewout-willem@hotmail.com>
2018-06-19 12:27:40 +0200

    routeKaarten update

--- src/main/java/client/ui/controllers/RouteCardController.java
+++ src/main/java/client/ui/controllers/RouteCardController.java
@@ -2,6 +2,7 @@ package client.ui.controllers;
 
 import game.GameStore;
 import game.GameStoreProvider;
+import game.routecards.RouteCard;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,6 +16,8 @@ public class RouteCardController {
     HBox Routecardbox;
     private Observable<GameStore> storeObervable = GameStoreProvider.getInstance();
     private GameStore store = GameStoreProvider.getStore();
+    private RouteCard[] routecardtemp = new RouteCard[5];
+    private RouteCard[] routecardchosen = new RouteCard[2];
 
     public void initialize() {
         // FillRouteCards();
@@ -31,18 +34,24 @@ public class RouteCardController {
     private void updateCard(int index) {
         ImageView imageView = (ImageView) Routecardbox.getChildren().get(index);
         imageView.setImage(new Image(getClass().getResourceAsStream(store.getSelectableRouteCards().getBank().getRandomRouteCard().getImagePath())));
+        routecardtemp[index] = store.getSelectableRouteCards().getBank().getRandomRouteCard();
     }
 
 
     public void isClicked(MouseEvent mouseEvent) {
         ImageView imageview = (ImageView) mouseEvent.getSource();
-        if ((mouseEvent.getClickCount()) % 2 == 1) {
+        int index = Integer.parseInt(imageview.getId());
+        if (routecardchosen[0] == null) {
+            routecardchosen[0] = routecardtemp[index];
             imageview.setStyle("-fx-effect: dropshadow(three-pass-box, green, 10, 10, 0, 0);");
-            ;
-
-        } else {
-            imageview.setStyle("");
+        } else if (routecardchosen[0] != null) {
+            routecardchosen[1] = routecardtemp[index];
+            imageview.setStyle("-fx-effect: dropshadow(three-pass-box, green, 10, 10, 0, 0);");
         }
+    }
 
+    public void ConfirmClicked(MouseEvent mouseEvent) {
+        store.getPlayerById(0).addRouteCard(routecardchosen[0]);
+        store.getPlayerById(0).addRouteCard(routecardchosen[1]);
     }
 }
