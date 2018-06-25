46fcd27e7a8d178ef5950b3a2f6e3ed69fcefe20
Thom Leenman <thomleenman@gmail.com>
2018-06-25 11:20:35 +0200

    ouregkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjk,kDSFFFFFFFFF> jv.    wfh welh,vnlknbivhj nsdNZXKLH:*t4uowesOUUYw4toiy4t'08y8240Y8w4wtlkjaweg;ouuETE;OHwet;oioawwe;gljlashdglkJSDGlkjagh;lahsgh;alsgflaskglaskjdfh,mcv z,skh v wouu wllwieubflkakrwfSDKBJJJJJJJJJJJJJJJJJJ nbjk uicvliugw4l3rtaew fsdvzcx kgj,nb 4wtaaarsdiuvgcxkjbn wtardfsUVCX lxkbn rawsfdvcx Control alt commit All praise the dictator System.shutdown(0);

--- src/main/java/client/ui/dialogs/RouteCardMessageController.java
+++ src/main/java/client/ui/dialogs/RouteCardMessageController.java
@@ -13,6 +13,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
@@ -29,6 +31,8 @@ public class RouteCardMessageController {
     private Image[] images = new Image[3];
     private RouteCard[] routeCards = new RouteCard[3];
     private boolean[] selected = new boolean[3];
+
+    private static final Logger Log = LogManager.getLogger(RouteCardMessageController.class);
 
     public void showDialog() {
         warningText.setText("");
